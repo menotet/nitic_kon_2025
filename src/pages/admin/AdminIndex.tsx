237ecf2117575d8a3d1fd744ea7a62948998ea86
menotet <megane8846493@gmail.com>
2025-09-30 17:26:02 +0900

    refactor: Change AdminIndex list

--- src/pages/admin/AdminIndex.tsx
+++ src/pages/admin/AdminIndex.tsx
@@ -1,21 +1,19 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const AdminIndex: React.FC = () => {
-    return (
-        <div>
-            <h1 className="text-3xl font-bold mb-4">Admin Dashboard</h1>
-            <p className="mb-4">Select a page to manage:</p>
-            <ul className="list-disc pl-5">
-                <li>
-                    <Link to="/admin/data" className="text-blue-300 underline">
-                        Timetable Data Management
-                    </Link>
-                </li>
-                {/* Add links to other admin pages here in the future */}
-            </ul>
-        </div>
-    );
+  return (
+    <div>
+      <h1 className="text-3xl font-bold mb-4">Admin Dashboard</h1>
+      <p className="mb-4">Select a page to manage:</p>
+      <div className="list-disc pl-5">
+        <Link to="/admin/data" className="text-blue-300 underline">
+          Timetable Data Management
+        </Link>
+        {/* Add links to other admin pages here in the future */}
+      </div>
+    </div>
+  );
 };
 
 export default AdminIndex;
